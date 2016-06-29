1d903f58cc8bf6915cde1ae9cce30aaf84bb52ae
Leonardo Miceli <elpacho@gmail.com>
2016-06-29 19:53:23 +0200

    added notification on new pictures found

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.java
@@ -1,5 +1,7 @@
 package com.bignerdranch.android.photogallery;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 
@@ -9,4 +11,9 @@ public class PhotoGalleryActivity extends SingleFragmentActivity {
     public Fragment createFragment() {
         return PhotoGalleryFragment.newInstance();
     }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
+
 }
--- app/src/main/java/com/bignerdranch/android/photogallery/network/PollService.java
+++ app/src/main/java/com/bignerdranch/android/photogallery/network/PollService.java
@@ -2,14 +2,20 @@ package com.bignerdranch.android.photogallery.network;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
+import com.bignerdranch.android.photogallery.PhotoGalleryActivity;
 import com.bignerdranch.android.photogallery.QueryPreferences;
+import com.bignerdranch.android.photogallery.R;
 import com.bignerdranch.android.photogallery.model.GalleryItem;
 
 import java.util.List;
@@ -17,7 +23,7 @@ import java.util.List;
 public class PollService extends IntentService {
 
     private static final String TAG = "PollService";
-    private static final int POLL_INTERVAL = 1000 * 60; // 60 seconds
+    private static final long POLL_INTERVAL = AlarmManager.INTERVAL_HALF_HOUR;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
@@ -70,7 +76,22 @@ public class PollService extends IntentService {
             Log.i(TAG, "Got an old result: " + resultId);
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+            NotificationManagerCompat notificationManager =
+                    NotificationManagerCompat.from(this);
+            notificationManager.notify(0, notification);
         }
+
         QueryPreferences.setLastResultId(this, resultId);
     }
 
